fc9cb23b0c49ae7acbe90b2e4766cd8b412b7a2d
oevlreyo <reyoyamiresu@yahoo.co.jp>
2019-04-23 20:17:14 +0900

    fix

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
-
+import os
 # botアカウントのトークンを指定
-# API_TOKEN = "xoxb-483283039265-615142416452-Kj4dqhjzmrBc2ast9Gv9b5Zd"
+API_TOKEN = os.environ['token']
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "「AC」または「レート」を含むメッセージを送信してください"
